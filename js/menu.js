28a956801c4afe381b31b405247e7804d535c785
Quang <tvq.dev@gmail.com>
2022-06-17 21:20:58 +0700

    push

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,8 @@ window.addEventListener("load", function () {
      const nav_close = document.querySelector(".nav_close");
      const nav_open = document.querySelector(".nav_open");
      const body = document.querySelector("body");
-
+     const recipes_heart = document.querySelector(".recipes_heart")
+     const recipes_heart_i = document.querySelector(".bxs-heart")
 
      links.forEach(item => item.addEventListener("mouseenter", handelHoverLink));
      const line = document.createElement("div");
@@ -30,4 +31,11 @@ window.addEventListener("load", function () {
      nav_close.addEventListener("click", () => {
           header_nav.classList.remove("active")
      })
+
+     recipes_heart.addEventListener("click", (e) => {
+          recipes_heart_i.classList.add("active")
+     })
+
+
+
 })
